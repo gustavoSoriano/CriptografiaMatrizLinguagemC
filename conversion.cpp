c1301579b96b270c0776f798a20082be084ac2be
gustavoSoriano <soriano97437091>
2016-11-21 15:05:53 -0200

    finalizado
    programa finalizado.

--- conversion.cpp
+++ conversion.cpp
@@ -3,15 +3,13 @@
 #include<conio.h>
 #include <string.h>
 
-
 #define DIM 3 //dimenções das matrizes
 
 void getCifra(int matriz[][DIM], char alfabeto[], char palavra[9]);
 void showMatrizInt(int matriz[][DIM]);
 void showMatrizFloat(float matriz[][DIM]);
 void criptografar(int matrizChave[][DIM], int matrizCifrada[][DIM], int criptografia[][DIM]);
-void descriptografar(float matrizChaveInversa[][DIM], int criptografia[][DIM]);
-
+void descriptografar(float matrizChaveInversa[][DIM], int criptografia[][DIM], int matrizCifrada[][DIM]);
 void desCifrar(int matriz[][DIM],  char alfabeto[]);
 
 int main()
@@ -22,19 +20,21 @@ int main()
     int   matrizCifrada[DIM][DIM];
     int   criptografia[DIM][DIM];
     
-    //vou inicializar o vetor para que possa validar posteriormente.
-    char op, palavra[9] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
-    
+    //vou inicializar o vetor para que possa validar posteriormente e completar os caracteres caso usr informe menos
+    char op, palavra[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
     
     do
     {
-      printf("\nEscolha\n 1. CRIAR CRIPTOGRAFIA\n 2. VER CRIPTOGRAFIA\n 3. DESCRIPTOGRAFAR\n 4. INFORMAR UMA CRIPTOGRAFIA\n 5.SAIR DO PROGRAMA\n ");    
+      printf("\nEscolha\n 1. CRIAR CRIPTOGRAFIA\n 2. VER CRIPTOGRAFIA\n 3. DESCRIPTOGRAFAR\n 4.SAIR DO PROGRAMA\n ");    
       op = getche(); 
          
       switch(op)
       {
           case '1':
                system("cls");
+               //apenas um macete para que a palavra nao pegue parte da criptografia anterior
+               for(int i=0; i<9; i++)palavra[i] = ' ';
+               
                printf("Informe uma palavra com 9 letras");
                fflush(stdin);
                gets(palavra);
@@ -45,9 +45,11 @@ int main()
 			   }
 			   else
 			   {
+                    //preciso substituir espaços por -                   
+                    for(int e=0; e<9; e++)if( palavra[e] == ' ' )palavra[e] = '-';
+
               	    //convertendo letras para numeros na ordem alfabetica
               	    getCifra(matrizCifrada, alfabeto, palavra);
-               
                     //gerando criptografia
                     criptografar(matrizChave, matrizCifrada, criptografia);
            		}
@@ -55,7 +57,7 @@ int main()
         
           case '2':
           	    system("cls");
-                if( palavra[0]  == NULL )
+                if( palavra[0]  == ' ' )
 			    {
 			   	    printf("Nao ha criptografia");
 			    }
@@ -67,33 +69,15 @@ int main()
         
           case '3':
           	    system("cls");
-                if( palavra[0]  == NULL )
+                if( palavra[0]  == ' ' )
 			    {
 			   	    printf("Nao ha criptografia");
 			    }
 			    else
 			    {   
-              		descriptografar(matrizChaveInversa, criptografia); 
+              		descriptografar(matrizChaveInversa, criptografia, matrizCifrada); 
               		desCifrar(matrizCifrada, alfabeto);
-          		}
-          break;
-          
-          case '4':
-              system("cls");
-              for( int l=0; l<3; l++ )
-              {
-                 for( int c=0; c<3; c++ )
-                 {
-                    printf(" Informe [%d][%d] ", l, c);
-                    scanf("%d", &matrizCifrada[l][c]);
-                 }  
-              } 
-              desCifrar(matrizCifrada, alfabeto);
-          break; 
-          
-          
-          case '5':
-              //sair do programa
+          		} 
           break;
           
           default:
@@ -101,49 +85,12 @@ int main()
           break;      
       }
        
-    }while(op != '6');
+    }while(op != '4');
     system("cls");
     printf("Ate mais !");
     
     system("pause");
     return 1;
-}
-
-
-
-
-//Recebe a palavra e retorna uma matriz na ordem numerica das letras
-void getCifra(int matriz[][DIM], char alfabeto[], char palavra[9])
-{
-    int ordem[9], indexV;
-      
-    //percorre todas as letras da palavra
-    for( int l=0; l<9; l++ )
-    {
-         //percorre alfabeto procurando a letra atual
-         for( int a=0; a<31; a++ )
-         {
-         
-            //faz comparação cada vez que percorrer o alfabeto
-            //o programa fica preso no segundo loop
-            //até percorrer todo alfabeto ou encontrar o caracter
-            if( palavra[l] == alfabeto[a] )
-            {
-                ordem[l] = a; //add ref. numerica dentro do vetor
-                a=31;         //apenas um flag para forçar a saida do laço
-            }
-         }   
-    }
-    
-    indexV=0;
-    //transferindo valores do vetor para matriz (preenchendo a matriz cifrada)
-    for( int l=0; l<3; l++ )
-    {
-         for( int c=0; c<3; c++ )
-         {
-            matriz[l][c] = ordem[indexV++];
-         }  
-    }
 }
 
 
@@ -175,12 +122,44 @@ void showMatrizFloat(float matriz[][DIM])
 }
 
 
+//Recebe a palavra e retorna uma matriz na ordem numerica das letras
+void getCifra(int matriz[][DIM], char alfabeto[], char palavra[9])
+{
+    int ordem[9], indexV;
+      
+    //percorre todas as letras da palavra
+    for( int l=0; l<9; l++ )
+    {
+         //percorre alfabeto procurando a letra atual
+         for( int a=0; a<31; a++ )
+         {
+            //faz comparação cada vez que percorrer o alfabeto
+            //o programa fica preso no segundo loop
+            //até percorrer todo alfabeto ou encontrar o caracter
+            if( palavra[l] == alfabeto[a] )
+            {
+                ordem[l] = a; //add ref. numerica dentro do vetor
+                a=31;         //apenas um flag para forçar a saida do laço
+            }
+         }   
+    }
+    
+    indexV=0;
+    //transferindo valores do vetor para matriz (preenchendo a matriz cifrada)
+    for( int l=0; l<3; l++ )
+    {
+         for( int c=0; c<3; c++ )
+         {
+            matriz[l][c] = ordem[indexV++];
+         }  
+    }
+}
+
 
 //realizar a multiplicação de matrizes gerando como resultado(produto) uma terceita matriz (criptografia)
 void criptografar(int matrizChave[][DIM], int matrizCifrada[][DIM], int criptografia[][DIM])
 {
     int somadora=0; // somar a multiplicação dos indices equivalentes entre as duas matrizes
-    
     for(int l=0; l<3; l++)
     {
         for(int c=0; c<3; c++)
@@ -189,7 +168,6 @@ void criptografar(int matrizChave[][DIM], int matrizCifrada[][DIM], int criptogr
             {
               somadora = (somadora) + ((matrizChave[l][j])*(matrizCifrada[j][c]));
             }
-            
             criptografia[l][c] = somadora;
             somadora=0;
         }
@@ -197,10 +175,9 @@ void criptografar(int matrizChave[][DIM], int matrizCifrada[][DIM], int criptogr
 }
 
 //descriptografando 
-void descriptografar(float matrizChaveInversa[][DIM], int criptografia[][DIM])
+void descriptografar(float matrizChaveInversa[][DIM], int criptografia[][DIM], int matrizCifrada[][DIM])
 { 
-    float somadora=0;
-    
+    float somadora=0.0;
     for(int l=0; l<3; l++)
     {
         for(int c=0; c<3; c++)
@@ -209,8 +186,8 @@ void descriptografar(float matrizChaveInversa[][DIM], int criptografia[][DIM])
             {
               somadora = (somadora) + ((matrizChaveInversa[l][j])*(criptografia[j][c]));
             }
-            printf(" [%.0f] ", somadora);
-            somadora=0;
+            printf(" [%d] ", somadora);
+            somadora=0;       
         }
         printf("\n");
     }
@@ -221,32 +198,27 @@ void descriptografar(float matrizChaveInversa[][DIM], int criptografia[][DIM])
 void desCifrar(int matriz[][DIM], char alfabeto[])
 {
     char  palavraDescriptografada[9];
-    int ordem[9], indexV=0;
+    int   indexV=0;
     
     //transferindo valores do vetor para matriz (preenchendo a matriz cifrada)
     for( int l=0; l<3; l++ )
     {
          for( int c=0; c<3; c++ )
          {
-            ordem[indexV++] = matriz[l][c];
-         }  
+             //percorre alfabeto procurando o indice
+             for( int a=0; a<31; a++ )
+             {
+                if( matriz[l][c] == a )
+                {
+                    palavraDescriptografada[indexV++] = alfabeto[a]; //add ref. numerica dentro do vetor
+                    a=31;                                            //apenas um flag para forçar a saida do laço
+                }
+             }   
+         }
     }
-    
-    
-    //percorre todos os numeros do vetor ordem
-    for( int l=0; l<9; l++ )
-    {
-         //percorre alfabeto procurando o indice
-         for( int a=0; a<31; a++ )
-         {
-            if( ordem[l] == a )
-            {
-                palavraDescriptografada[l] = alfabeto[a]; //add ref. numerica dentro do vetor
-                a=31;                           //apenas um flag para forçar a saida do laço
-            }
-         }   
-    }
-    
+
+    //preciso devolver os espaços que foram substituidos por -                   
+    for(int e=0; e<9; e++)if( palavraDescriptografada[e] == '-' )palavraDescriptografada[e] = ' ';
     system("cls");
     printf("\n esta eh a palavra que foi criptografada %s \n", palavraDescriptografada);
 }
